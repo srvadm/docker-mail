1296cacd29680dd3e49de8a21d7b1cc67606119f
tinonaumann <tino@naumix.de>
2020-03-24 11:56:52 +0200

    u

--- nginx/rootfs/docker-entrypoint.sh
+++ nginx/rootfs/docker-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if ! [ -f "/var/www/configs/nginx/host.conf" ]; then
-  touch /var/www/configs/nginx/host.conf
-  chown 1000:1000 /var/www/configs/nginx/host.conf
-fi
-
 if [ -z ${DOMAIN-} ]; then
   echo you need to define a domain
   exit 1
