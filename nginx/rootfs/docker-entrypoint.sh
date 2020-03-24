b35731c7be5a2e0decaa5bfebaf4fed19def2a33
tinonaumann <tino@naumix.de>
2020-03-24 11:48:25 +0200

    u

--- nginx/rootfs/docker-entrypoint.sh
+++ nginx/rootfs/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if ! [ -f "/var/www/configs/nginx/host.conf" ]; then
   touch /var/www/configs/nginx/host.conf
-#  chown 1000:1000 /var/www/configs/nginx/host.conf
+  chown 1000:1000 /var/www/configs/nginx/host.conf
 fi
 
 if [ -z ${DOMAIN-} ]; then
