3b162ef897f45150d356f1a3f040381b6bda64f2
tinonaumann <tino@naumix.de>
2020-03-24 19:25:05 +0200

    u

--- nginx/rootfs/docker-entrypoint.sh
+++ nginx/rootfs/docker-entrypoint.sh
@@ -4,6 +4,18 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+mkdir -p                            \
+  /var/www/html/public/             \
+  /var/www/logs/nginx/              \
+  /var/www/configs/nginx/           \
+&& touch                            \
+  /var/www/configs/nginx/host.conf  \
+&& chown 1000:1000                  \
+  /var/www/html/public/             \
+  /var/www/logs/nginx/              \
+  /var/www/configs/nginx/           \
+  /var/www/configs/nginx/host.conf
+
 if [ -z ${DOMAIN-} ]; then
   echo you need to define a domain
   exit 1
