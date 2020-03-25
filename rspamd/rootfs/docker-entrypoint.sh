509b3d1cbf6c04e09d017c7d9c11267c940cef48
tinonaumann <tino@naumix.de>
2020-03-25 23:19:01 +0200

    u

--- rspamd/rootfs/docker-entrypoint.sh
+++ rspamd/rootfs/docker-entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-
-cat << EOF > /etc/rspamd/override.d/worker-proxy.inc
-bind_socket = "*:11332";
-milter = yes;
-timeout = 120s;
-upstream "local" {
-  default = yes;
-  self_scan = yes;
+cat << EOF > /etc/rspamd/override.d/milter_headers.conf
+extended_spam_headers = true;
+EOF
+cat << EOF > /etc/rspamd/override.d/classifier-bayes.conf
+autolearn = true;
+users_enabled = true;
+EOF
+cat << EOF > /etc/rspamd/override.d/redis.conf
+servers = "$REDIS_SERVER";
+EOF
+cat << EOF > /etc/rspamd/override.d/statistic.conf
+classifier "bayes" {
+   users_enabled = true;
+   backend = "redis";
 }
 EOF
-
 
 "$@"
