f99949b0d2207d8d2d967e6e0b270ae49b0c022b
tinonaumann <tino@naumix.de>
2020-03-25 19:41:06 +0200

    u

--- rspamd/rootfs/docker-entrypoint.sh
+++ rspamd/rootfs/docker-entrypoint.sh
@@ -16,4 +16,17 @@ classifier "bayes" {
    backend = "redis";
 }
 EOF
+cat << EOF > /etc/rspamd/local.d/worker-proxy.inc
+upstream "local" {
+  self_scan = yes;
+}
+bind_socket = *:11332;
+EOF
+cat << EOF > /etc/rspamd/local.d/worker-proxy.inc
+upstream "local" {
+  self_scan = yes;
+}
+bind_socket = *:11332;
+EOF
+
 "$@"
