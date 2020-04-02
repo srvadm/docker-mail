759ede673ab2834e5c9bf4f23aaba897d6e7f902
tinonaumann <tino@naumix.de>
2020-04-02 20:27:15 +0200

    u

--- nginx/rootfs/docker-entrypoint.sh
+++ nginx/rootfs/docker-entrypoint.sh
@@ -8,6 +8,8 @@ mkdir -p                            \
   /var/www/html/public/             \
   /var/www/logs/nginx/              \
   /var/www/configs/nginx/           \
+&& touch                            \
+  /var/www/configs/nginx/host.conf  \
 && chown 1000:1000                  \
   /var/www/html/public/             \
   /var/www/logs/nginx/              \
